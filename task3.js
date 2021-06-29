9db5e4cf95ebdaf347aef260a542f19b57b3b5de
aliefabdussalam <83624142+aliefabdussalam@users.noreply.github.com>
2021-06-29 13:17:08 +0700

    changes

--- task3.js
+++ task3.js
@@ -1,13 +1,26 @@
 const numericSort = (n1, n2, data) =>{
-    if (n1 < n2 && data > 5) {
-    const num = data.sort((a, b)=>{return a - b})
-    return `${num.filter(c => c > n1 && c < n2)}`
+    //validasi persyaratan
+    if (n1 < n2 && data.length > 5) {
+    //sort
+    
+    //filter apabila persyaratan tercapai
+    const output = data.filter(
+        (c = (data) => {
+          return data  > n1 && data < n2;
+        })
+      );
+      return output.sort((a, b)=>{return a-b})
     }
+    //apabila nilai awal lebih besar dari niai akhir
     else if(n1 > n2){
         return "Nilai akhir harus lebih besar dari nilai awal"
     }
+    //apabila data kurang dari 5
     else if(data < 5){
         return "Jumlah angka dalam dataArray tidak sesuai"
     }
+    else{
+        return "Jumlah angka dalam dataArray tidak ada"
+    }
 }
-console.log(numericSort(15, 3 , [2, 25, 4, 14, 17, 30, 8]))
+console.log(numericSort(5, 30 , [2, 25, 4, 14, 17, 30, 8]))
